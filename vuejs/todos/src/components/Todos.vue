aca5099b0cc21ff93ec13e180ab2ecea8d8c8a56
Marcos Rivas Rojas <marivr@microsoft.com>
2020-04-26 21:46:33 -0500

    ejemplo vuejs

---
+++ vuejs/todos/src/components/Todos.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div v-bind:key="todo.id" v-for="todo in todos">
+            <Todo 
+                v-bind:todo="todo" 
+                v-on:delete-todo="$emit('delete-todo',todo.id)" />
+        </div>
+    </div>
+</template>
+
+
+<script>
+
+import Todo from './TodoItem'
+
+export default {
+    name: 'Todos',
+    props: ['todos'],
+    components:{Todo}
+}
+</script>
+
+<style scoped>
+
+</style>
